52bc95314140e8361407212a39f421fbd93fb31e
PiotrLasz <56960121+PiotrLasz@users.noreply.github.com>
2019-11-26 14:03:35 +0000

    Update script.js

--- script/script.js
+++ script/script.js
@@ -15,32 +15,105 @@ $(document).ready(function() {
                 table +='<td>';
                 table +='R:' + i + ' C:' + j;
                 table +='</td>';
+
             }
 
             table += '</tr>';
+
         }
 
         table +='</table>';
 
         $('.Board').html(table);
 
-        function addWalls() {
+        function addElements() {
 
             let allTiles = document.getElementsByTagName('td');
-            
 
+            function randomTile() {
+                return Math.floor(Math.random()*allTiles.length);
+            }
+            
             for (let i=0; i<5; i++) {
 
-                let x = Math.floor(Math.random()*allTiles.length);
+                let x = randomTile();
                 
-                $('td').eq(x).addClass('Wall');
+                if ($('td').eq(x).hasClass('Wall')) {
+
+                    i -= 1;
+
+                } else {
+
+                    $('td').eq(x).addClass('Wall');
+
+                }
+
+            }
+
+            for (let j=0; j<5; j++) {
+
+                let y = randomTile();
+
+                if ($('td').eq(y).hasClass('Wall') || $('td').eq(y).hasClass('Cash')) {
+
+                    j -= 1;
+
+                } else {
+
+                    $('td').eq(y).replaceWith('<td class="Cash">£60</td>');
+
+                }
+
+            }
+
+            for (let k=0; k<1; k++) {
+
+                let z = randomTile();
+
+                if($('td').eq(z).hasClass('Wall') || $('td').eq(z).hasClass('Cash')) {
+
+                    k -= 1;
+
+                } else {
+
+                    $('td').eq(z).replaceWith('<td class="Player">O_O</td>');
+
+                }
 
             }
 
         };
 
-        addWalls();
+        addElements();
 
     });
+
+    $(document.getElementsByClassName('Player')).keydown(function(e) {
+
+        switch(e.which) {
+
+            case 37: //Left
+                
+            break;
+
+            case 38: //Up
+
+            break;
+
+            case 39: //Right
+
+            break;
+
+            case 40: //Down
+
+            break;
+
+            default: return;
+
+        }
+
+        e.preventDefault();
+
+    })
 
 });
